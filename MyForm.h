5ea93592d9f82fafb43ecd13e3cb9acb77bb32e8
Bashar-A <basharmamma@hotmail.com>
2020-02-26 16:40:55 +0300

    V1.2.1.1

--- MyForm.h
+++ MyForm.h
@@ -794,6 +794,7 @@ private: System::Void Button1_Click(System::Object^ sender, System::EventArgs^ e
 private: System::Void TbInput_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	int gittest = 2;
 	int gittest2 = 1;
+	int gittest3 = 1;
 	textBox1->Text = "";
 	textBox2->Text = "";
 	textBox3->Text = "";
